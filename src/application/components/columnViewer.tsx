555f0d450841febc26b3c5df34bfe964dc631ff7
Alexis Thual <alexisthual@gmail.com>
2018-10-28 20:21:17 +0100

    Add 'add column' button

--- src/application/components/columnViewer.tsx
+++ src/application/components/columnViewer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {ControlGroup, InputGroup, Switch} from '@blueprintjs/core';
+import {Icon, ControlGroup, InputGroup, Switch} from '@blueprintjs/core';
 
 import {columnExamples} from '../mockdata/database'
 
@@ -16,16 +16,21 @@ export default class ColumnViewer extends React.Component<IColumnViewerProps, IC
         let {dispatch} = this.props
 
         let columns = columnExamples.map((column: any) =>
-            <div className={'column'}>
-                <div className={'column-header'}>
-                    <div>{column.owner}</div>
-                    <div>{column.table}</div>
-                    <div>{column.column}</div>
+            <div>
+                <div className={'add-button'}>
+                    <Icon icon={'plus'}/>
                 </div>
-                <div className={'column-rows'}>
-                    {column.rows.map((row: string) =>
-                        <div>{row}</div>
-                    )}
+                <div className={'column'}>
+                    <div className={'column-header'}>
+                        <div>{column.owner}</div>
+                        <div>{column.table}</div>
+                        <div>{column.column}</div>
+                    </div>
+                    <div className={'column-rows'}>
+                        {column.rows.map((row: string) =>
+                            <div>{row}</div>
+                        )}
+                    </div>
                 </div>
             </div>
         )
