ec9f5a9c155a8bc50aa691675412861b1d152267
Alexis Thual <alexisthual@gmail.com>
2018-10-31 13:37:11 +0100

    Add mapping mock data for testing webapp

---
+++ src/application/mockdata/mappings.tsx
@@ -0,0 +1,23 @@
+import {IMapping} from '../types'
+
+export const cw_patient_mapping: IMapping = {
+    pathToPrimaryKey: {
+        owner: 'ISCF',
+        table: 'PATIENT',
+        column: 'NOPAT',
+    },
+    fhirMapping: {
+        'Name.FirstName': {
+            inputColumns: [
+                {
+                    owner: 'ISCF',
+                    table: 'PATIENT',
+                    column: 'PREPAT',
+                    join: null,
+                    script: 'parsePrenom.py'
+                }
+
+            ]
+        }
+    }
+}
