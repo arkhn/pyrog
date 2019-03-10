9b27e4333d08ad2cf5ceb09e600ab00d0fd076c2
alexis <alexisthual@gmail.com>
2019-03-10 01:25:27 +0100

    Working tests for server

--- test/schema.test.js
+++ test/schema.test.js
@@ -1,6 +1,6 @@
 import { graphql } from 'graphql'
 import { importSchema } from 'graphql-import'
-import { makeExecutableSchema } from 'graphql-tools'
+import { makeExecutableSchema, mockServer } from 'graphql-tools'
 import { GraphQLServer } from 'graphql-yoga'
 
 import { Prisma as PrismaClient } from '../src/generated/prisma-client'
@@ -8,10 +8,12 @@ import { Prisma as PrismaBinding } from '../src/generated/prisma-binding'
 import resolvers from '../src/resolvers'
 
 const endpoint = 'http://localhost:4466'
+const debug = false
 
 const prismaBindingInstance = () => {
     return new PrismaBinding({
         endpoint,
+        debug,
     })
 }
 
@@ -50,6 +52,7 @@ describe('Prisma binding', () => {
 
 const prismaClientInstance = new PrismaClient({
     endpoint,
+    debug,
 })
 
 describe('Prisma client', () => {
@@ -77,11 +80,11 @@ const serverInstance = new GraphQLServer({
         ...request,
         binding: new PrismaBinding({
             endpoint,
-            debug: true,
+            debug,
         }),
         client: new PrismaClient({
             endpoint,
-            debug: true,
+            debug,
         }),
     }),
     resolverValidationOptions: {
@@ -89,25 +92,41 @@ const serverInstance = new GraphQLServer({
     },
 })
 
-describe.skip('Graphql server', () => {
+const sendQuery = async (query, variables) => {
+    return await fetch('http://0.0.0.0:4000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query,
+            variables,
+        })
+    }).then(response => {
+        return response.json()
+    })
+}
+
+describe('Graphql server', () => {
     let server
-    let executableSchema
 
     beforeAll(async () => {
-        // console.log(serverInstance)
-        const typeDefs = importSchema('./src/schema.graphql')
-        executableSchema = makeExecutableSchema({ typeDefs })
         server = await serverInstance.start()
     })
 
-    test('Query allDatabases', async () => {
-        expect(
-            await graphql(serverInstance.executableSchema, `query {allDatabases { id }}`)
-            // await graphql(executableSchema, `query {allDatabases { id }}`)
-            // await server.post(`query {allDatabases { id }}`)
-        ).toEqual(expect.arrayContaining([{
-            name: "Crossway"
-        }]))
+    describe('Queries', () => {
+        test('allDatabases', async () => {
+            expect(
+                await sendQuery(`query { allDatabases { id name }}`, {})
+            ).toEqual({
+                data: {
+                    allDatabases: expect.arrayContaining([
+                        expect.objectContaining({ name: "Crossway" })
+                    ])
+                }
+            })
+        })
     })
 
     afterAll(() => {
