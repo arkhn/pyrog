b1be312e6fc071217fff15a7a1a293908efaa079
alexis <alexisthual@gmail.com>
2019-03-16 21:15:55 +0100

    Add upload progress bar ; allow upload cancelling

--- src/application/containers/views/newSource/index.tsx
+++ src/application/containers/views/newSource/index.tsx
@@ -2,9 +2,13 @@ import {
     Button,
     Code,
     FileInput,
+    FormGroup,
     InputGroup,
+    IToastProps,
     Position,
+    ProgressBar,
 } from '@blueprintjs/core'
+import axios from 'axios'
 import * as React from 'react'
 import {
     Mutation,
@@ -19,6 +23,7 @@ import Navbar from '../../utils/navbar'
 
 // Import types
 import {
+    IDatabase,
     IReduxStore,
     IView,
 } from '../../../types'
@@ -29,6 +34,9 @@ import './style.less'
 
 // GRAPHQL OPERATIONS
 
+// Queries
+const allDatabases = require('./graphql/queries/allDatabases.graphql')
+
 // Mutations
 const mutationLogin = require('./graphql/mutations/login.graphql')
 
@@ -37,6 +45,7 @@ export interface INewSourceState {
 }
 
 interface IState {
+    isUploading: boolean,
     sourceName: string,
     schemaFile: any,
 }
@@ -67,6 +76,7 @@ class NewSourceView extends React.Component<INewSourceViewState, IState> {
     constructor(props: INewSourceViewState) {
         super(props)
         this.state = {
+            isUploading: false,
             sourceName: "",
             schemaFile: null,
         }
@@ -81,24 +91,81 @@ class NewSourceView extends React.Component<INewSourceViewState, IState> {
         const formData = new FormData()
         formData.append('schema', file, this.state.sourceName)
 
-        return fetch(`${SCHEMA_URL}/upload`, {
-            headers: {},
-            method: 'POST',
-            body: formData,
+        this.setState({
+            ...this.state,
+            isUploading: true,
         })
-        .then((response: any) => {
-            return response.json()
-        }).then((response: any) => {
-            this.props.toaster.show({
-                icon: response.success ? 'upload' : 'cross',
-                message: response.message,
-                intent: response.success ? 'success' : 'danger',
-                timeout: 4000,
-            })
 
-            if (response.success) {
+        const renderToastProps = ({ action, uploadProgress, success, message }: any): IToastProps => {
+            return {
+                action,
+                icon: "cloud-upload",
+                intent: typeof uploadProgress !== 'undefined' ? null : (success ? 'success' : 'danger'),
+                message: (uploadProgress ?
+                    <ProgressBar
+                        intent={uploadProgress < 1 ? 'primary' : 'success'}
+                        value={uploadProgress}
+                    /> :
+                    message),
+                timeout: uploadProgress < 1 ? 0 : 2500,
+            }
+        }
+
+        const CancelToken = axios.CancelToken
+        let cancel: any
+
+        const key = this.props.toaster.show(renderToastProps({
+            action: {
+                onClick: () => {
+                    cancel()
+                },
+                text: 'Interrompre',
+            },
+            uploadProgress: 0,
+        }))
+
+        return axios.request({
+            cancelToken: new CancelToken((c: any) => {
+                cancel = c;
+            }),
+            data: formData,
+            method: "post",
+            url: `${SCHEMA_URL}/upload`,
+            onUploadProgress: (p) => {
+                this.props.toaster.show(renderToastProps({
+                    action: {
+                        onClick: () => {
+                            cancel()
+                        },
+                        text: 'Interrompre',
+                    },
+                    uploadProgress: p.loaded / p.total,
+                }), key)
+            }
+        }).then((response: any) => {
+            this.props.toaster.show(renderToastProps({
+                message: response.data.message,
+                success: response.data.success,
+            }), key)
+
+            if (response.data.success) {
+                this.setState({
+                    ...this.state,
+                    isUploading: false,
+                })
+
                 this.props.history.push('/sources')
             }
+        }).catch((response: any) => {
+            this.setState({
+                ...this.state,
+                isUploading: false,
+            })
+
+            this.props.toaster.show(renderToastProps({
+                message: 'Annulé',
+                success: false,
+            }), key)
         })
     }
 
@@ -109,6 +176,7 @@ class NewSourceView extends React.Component<INewSourceViewState, IState> {
         } = this.props
 
         const {
+            isUploading,
             sourceName,
             schemaFile,
         } = this.state
@@ -132,64 +200,82 @@ class NewSourceView extends React.Component<INewSourceViewState, IState> {
         return <div>
             <Navbar />
             <div id='main-container-newsource'>
-                <form
-                    onSubmit={this.onFormSubmit}
+                <Query
+                    query={allDatabases}
                 >
-                    <h1>Nom de la source</h1>
-                    <InputGroup
-                        onChange={(event: React.FormEvent<HTMLElement>) => {
-                            const target = event.target as HTMLInputElement
+                    {({ data, loading}) => {
+                        const databaseNames = data.allDatabases ?
+                            data.allDatabases.map((database: IDatabase) => database.name) :
+                            null
 
-                            this.setState({
-                                ...this.state,
-                                sourceName: target.value,
-                            })
-                        }}
-                        name={'sourceName'}
-                        placeholder="Nom de la source..."
-                        value={sourceName}
-                    />
+                        return <form
+                            onSubmit={this.onFormSubmit}
+                        >
+                            <h1>Nom de la source</h1>
+                            <FormGroup
+                                helperText={databaseNames && databaseNames.indexOf(sourceName) >= 0 ? <p className={'warning'}>Ce nom existe déjà et n'est pas disponible.</p>  : null}
+                            >
+                                <InputGroup
+                                    onChange={(event: React.FormEvent<HTMLElement>) => {
+                                        const target = event.target as HTMLInputElement
 
-                    <h1>Schéma de la base</h1>
-                    <p>
-                        Une source de données doit nécessairement être importée avec un schéma de données SQL. Ce schéma doit être importé au format JSON et organisé comme suit :
-                    </p>
-                    <pre>
-                        <code dangerouslySetInnerHTML={{__html: schemaType}} />
-                    </pre>
-                    <p>
-                        Voici un exemple minimaliste de fichier représentant un schéma de base de données JSON tel qu'il est requis par notre application à l'heure actuelle :
-                    </p>
-                    <pre>
-                        <code dangerouslySetInnerHTML={{__html: schemaExample}} />
-                    </pre>
-                    <FileInput
-                        fill
-                        inputProps={{
-                            onChange: (event: React.FormEvent<HTMLInputElement>) => {
-                                const target = event.target as any
-                                this.setState({
-                                    ...this.state,
-                                    schemaFile: target.files[0],
-                                })
-                            },
-                            name: 'schema',
-                        }}
-                        text={schemaFile ?
-                            schemaFile.name :
-                            <p className='disabled-text'>Importer un schéma...</p>
-                        }
-                    />
-                    <br/>
-                    <Button
-                        disabled={!sourceName || !schemaFile}
-                        intent='primary'
-                        large
-                        type="submit"
-                    >
-                        Ajouter
-                    </Button>
-                </form>
+                                        this.setState({
+                                            ...this.state,
+                                            sourceName: target.value,
+                                        })
+                                    }}
+                                    name={'sourceName'}
+                                    placeholder="Nom de la source..."
+                                    value={sourceName}
+                                />
+                            </FormGroup>
+
+
+                            <h1>Schéma de la base *</h1>
+                            <FileInput
+                                fill
+                                inputProps={{
+                                    onChange: (event: React.FormEvent<HTMLInputElement>) => {
+                                        const target = event.target as any
+                                        this.setState({
+                                            ...this.state,
+                                            schemaFile: target.files[0],
+                                        })
+                                    },
+                                    name: 'schema',
+                                }}
+                                text={schemaFile ?
+                                    schemaFile.name :
+                                    <p className='disabled-text'>Importer un schéma...</p>
+                                }
+                            />
+                            <br/>
+                            <div className="align-right">
+                                <Button
+                                    disabled={!databaseNames || databaseNames.indexOf(sourceName) >= 0 || !sourceName || !schemaFile || isUploading}
+                                    intent='primary'
+                                    large
+                                    type="submit"
+                                >
+                                    Ajouter
+                                </Button>
+                            </div>
+
+                            <p>
+                                * Une source de données doit nécessairement être importée avec un schéma de données SQL. Ce schéma doit être importé au format JSON et organisé comme suit :
+                            </p>
+                            <pre>
+                                <code dangerouslySetInnerHTML={{__html: schemaType}} />
+                            </pre>
+                            <p>
+                                Voici un exemple minimaliste de fichier représentant un schéma de base de données JSON tel qu'il est requis par notre application à l'heure actuelle :
+                            </p>
+                            <pre>
+                                <code dangerouslySetInnerHTML={{__html: schemaExample}} />
+                            </pre>
+                        </form>
+                    }}
+                </Query>
             </div>
         </div>
     }
