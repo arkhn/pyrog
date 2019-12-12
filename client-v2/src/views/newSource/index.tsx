57d0ec9978c56163d2dc548702f4688a86bcd29b
Jasopaum <jason.paumier@gmail.com>
2019-12-12 09:52:43 +0100

    Put back DB schema upload
    routes copy-pasted from server v1

--- client-v2/src/views/newSource/index.tsx
+++ client-v2/src/views/newSource/index.tsx
@@ -1,9 +1,13 @@
 import {
   Button,
   Checkbox,
+  FileInput,
   FormGroup,
   InputGroup,
+  IToastProps,
+  ProgressBar,
 } from '@blueprintjs/core';
+import axios from "axios";
 import * as React from 'react';
 import { Query, withApollo } from 'react-apollo';
 import { connect } from 'react-redux';
@@ -24,10 +28,12 @@ const mCreateSource = require('../../graphql/mutations/createSource.graphql');
 export interface INewSourceState {}
 
 interface IState {
+  hasOwner: boolean;
+  isUploading: boolean;
   templateName: string;
   templateExists: boolean;
   sourceName: string;
-  hasOwner: boolean;
+  schemaFile: any;
 }
 
 interface INewSourceViewState extends IView, INewSourceState {}
@@ -45,49 +51,195 @@ class NewSourceView extends React.Component<INewSourceViewState, IState> {
   constructor(props: INewSourceViewState) {
     super(props);
     this.state = {
+      hasOwner: false,
+      isUploading: false,
       templateName: '',
       templateExists: false,  // TODO check if really needed in state
       sourceName: '',
-      hasOwner: false,
+      schemaFile: null,
     };
   }
 
   onFormSubmit = async (e: any) => {
     e.preventDefault();
 
-    // Create new template in Graphql if it doesn't exist
-    if (!this.state.templateExists) {
-      await this.props.client
-        .mutate({
-          mutation: mCreateTemplate,
-          variables: {
-            name: this.state.templateName,
-          }
-        })
-    }
+    const formData = new FormData();
+    formData.append("schema", this.state.schemaFile, this.state.sourceName);
 
-    // Create new source in Graphql
-    this.props.client
-      .mutate({
-        mutation: mCreateSource,
-        variables: {
-          templateName: this.state.templateName,
-          name: this.state.sourceName,
-          hasOwner: this.state.hasOwner,
+    this.setState({
+      ...this.state,
+      isUploading: true
+    });
+
+    const renderToastProps = ({
+      action,
+      uploadProgress,
+      success,
+      message
+    }: any): IToastProps => {
+      return {
+        action,
+        icon: "cloud-upload",
+        intent:
+          typeof uploadProgress !== "undefined"
+            ? null
+            : success
+            ? "success"
+            : "danger",
+        message: uploadProgress ? (
+          <ProgressBar
+            intent={uploadProgress < 1 ? "primary" : "success"}
+            value={uploadProgress}
+          />
+        ) : (
+          message
+        ),
+        timeout: uploadProgress < 1 ? 0 : 2500
+      };
+    };
+
+    const CancelToken = axios.CancelToken;
+    let cancel: any;
+
+    const key = this.props.toaster.show(
+      renderToastProps({
+        action: {
+          onClick: () => {
+            cancel();
+          },
+          text: "Interrompre"
+        },
+        uploadProgress: 0
+      })
+    );
+
+    return axios
+      .request({
+        cancelToken: new CancelToken((c: any) => {
+          cancel = c;
+        }),
+        data: formData,
+        method: "post",
+        url: `${process.env.HTTP_BACKEND_URL}/upload`,
+        onUploadProgress: p => {
+          this.props.toaster.show(
+            renderToastProps({
+              action: {
+                onClick: () => {
+                  cancel();
+                },
+                text: "Interrompre"
+              },
+              uploadProgress: p.loaded / p.total
+            }),
+            key
+          );
         }
       })
-      .then((graphqlResponse: any) => {
-        // After source is created,
-        // redirect to /sources page.
-        this.props.history.push('/sources');
+      .then(async (response: any) => {
+        // Create new source in Graphql
+        if (response.data.success) {
+          // Create new template in Graphql if it doesn't exist
+          if (!this.state.templateExists) {
+            await this.props.client
+              .mutate({
+                mutation: mCreateTemplate,
+                variables: {
+                  name: this.state.templateName,
+                }
+              })
+          }
+          this.props.client
+            .mutate({
+              mutation: mCreateSource,
+              variables: {
+                templateName: this.state.templateName,
+                name: this.state.sourceName,
+                hasOwner: this.state.hasOwner,
+              }
+            })
+            .then((graphqlResponse: any) => {
+              // After source is created,
+              // redirect to /sources page.
+              this.props.toaster.show(
+                renderToastProps({
+                  message: response.data.message,
+                  success: response.data.success
+                }),
+                key
+              );
+
+              this.setState({
+                ...this.state,
+                isUploading: false
+              });
+
+              this.props.history.push("/sources");
+            })
+            .catch((error: any) => {
+              console.log(error);
+            });
+        } else {
+          this.props.toaster.show(
+            renderToastProps({
+              message: response.data.message,
+              success: response.data.success
+            }),
+            key
+          );
+        }
       })
-      .catch((error: any) => {
-        console.log(error);
+      .catch((response: any) => {
+        this.setState({
+          ...this.state,
+          isUploading: false
+        });
+
+        this.props.toaster.show(
+          renderToastProps({
+            message: "Annulé",
+            success: false
+          }),
+          key
+        );
       });
   };
 
   public render = () => {
-    const { templateName, sourceName } = this.state;
+    const { isUploading, templateName, sourceName, schemaFile } = this.state;
+
+    const schemaType: string = `
+    [owner: string] : {
+        [table: string]: string[]
+    }`;
+    
+        const schemaExample: string = `
+    {
+        "$SYSTEM": {
+            "PATIENT": [
+                "ID",
+                "PRENOM",
+                "NOM"
+            ]
+        },
+    }`;
+    
+        const sqlCommand: string = `
+    SELECT OWNER, TABLE_NAME, COLUMN_NAME, DATA_TYPE FROM all_tab_columns;
+    `;
+    
+        const sqlplusCommand: string = `
+    set heading off;
+    set underline off;
+    set pagesize 0;
+    set colsep ";";
+    set trimspool on;
+    set headsep off;
+    set linesize 1000;
+    set numw 64;
+    spool schema_name.csv
+    SELECT OWNER, TABLE_NAME, COLUMN_NAME, DATA_TYPE FROM all_tab_columns;
+    `;
 
     return (
       <div>
@@ -151,6 +303,28 @@ class NewSourceView extends React.Component<INewSourceViewState, IState> {
                       value={sourceName}
                     />
                   </FormGroup>
+
+                  <h1>Schéma de la base *</h1>
+                  <FileInput
+                    fill
+                    inputProps={{
+                      onChange: (event: React.FormEvent<HTMLInputElement>) => {
+                        const target = event.target as any;
+                        this.setState({
+                          ...this.state,
+                          schemaFile: target.files[0]
+                        });
+                      },
+                      name: "schema"
+                    }}
+                    text={
+                      schemaFile ? (
+                        schemaFile.name
+                      ) : (
+                        <p className="disabled-text">Importer un schéma...</p>
+                      )
+                    }
+                  />
                   <br />
                   <Checkbox
                     checked={this.state.hasOwner}
@@ -177,6 +351,38 @@ class NewSourceView extends React.Component<INewSourceViewState, IState> {
                       Ajouter
                     </Button>
                   </div>
+
+                  <p>
+                    * Une source de données doit nécessairement être importée
+                    avec un schéma de données SQL. Ce schéma doit être importé
+                    au format JSON et organisé comme suit :
+                  </p>
+                  <pre>
+                    <code dangerouslySetInnerHTML={{ __html: schemaType }} />
+                  </pre>
+                  <p>
+                    Voici un exemple minimaliste de fichier représentant un
+                    schéma de base de données JSON tel qu'il est requis par
+                    notre application à l'heure actuelle :
+                  </p>
+                  <pre>
+                    <code dangerouslySetInnerHTML={{ __html: schemaExample }} />
+                  </pre>
+                  <p>
+                    Le schéma de données peut-être extrait en utilisant le
+                    logiciel Toad avec cette commande SQL :
+                  </p>
+                  <pre>
+                    <code dangerouslySetInnerHTML={{ __html: sqlCommand }} />
+                  </pre>
+                  <p>
+                    ou en ligne de commande avec <code>sqlplus</code> :
+                  </p>
+                  <pre>
+                    <code
+                      dangerouslySetInnerHTML={{ __html: sqlplusCommand }}
+                    />
+                  </pre>
                 </form>
               );
             }}
--- server-v2/src/server.ts
+++ server-v2/src/server.ts
@@ -5,11 +5,79 @@ import { permissions } from 'permissions'
 import { schema } from './schema'
 import { createContext } from './context'
 
-new GraphQLServer({
+import * as multer from 'multer'
+import * as fs from 'fs'
+
+// TODO put db schemas in a folder
+const SCHEMAS_DEST = `${__dirname}` //`${process.env.STATIC_FILES_DIR}/schemas`;
+
+const server = new GraphQLServer({
   schema,
   context: createContext,
   middlewares: [permissions],
-}).start(() =>
+})
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, `${SCHEMAS_DEST}/`);
+  },
+  filename(req, file, cb) {
+    let name = `${file.originalname}.json`;
+
+    if (fs.existsSync(`${SCHEMAS_DEST}/${name}`)) {
+      name = `${(Math.random() + 1).toString(36).substring(2, 5)}.json`;
+    }
+
+    cb(null, name);
+  }
+});
+
+var upload = multer({
+  limits: {
+    // Maximum file size in bytes (here, 100Mo)
+    fileSize: 100 * 1024 * 1024
+  },
+  storage
+});
+
+server.use(function(req: any, res: any, next: any) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+server.post('/upload', upload.single('schema'), function(req: any, res: any) {
+  console.log(
+    req.file && req.file.originalname
+      ? `Uploading ${req.file.originalname}.json...`
+      : 'No File Uploaded'
+  );
+
+  res.send({
+    message:
+      req.file && req.file.originalname
+        ? 'Schéma uploadé'
+        : 'Une erreur est survenue',
+    success: req.file && req.file.originalname
+  });
+});
+
+server.get('/schemas/:filename', function(req: any, res: any) {
+  res.sendFile(
+    `${__dirname}/${req.params.filename}`,
+    (err: any) => {
+      if (err) {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(err.status).send({ error: err, message: err.message });
+      }
+    }
+  );
+});
+
+server.start(() =>
   console.log(
     `🚀 Server ready at: http://localhost:4000\n⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️`,
   ),
