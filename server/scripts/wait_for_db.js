527fe2a8f8ce3770dca38da1bef920017b6366f6
Theo <theo.leffyr@gmail.com>
2020-01-03 17:34:04 +0100

    Update Dockerfile to wait for prisma

---
+++ server/scripts/wait_for_db.js
@@ -0,0 +1,35 @@
+const { Client } = require('pg');
+const connectionString = process.env.POSTGRES_URL;
+const client = new Client({
+  connectionString: connectionString
+});
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+const wait = async () => {
+  let ok = false;
+  for (let index = 0; index < 15; index++) {
+    try {
+      await client.connect();
+      ok = true;
+      break;
+    } catch {
+      console.log('.');
+      await sleep(1000);
+      continue;
+    }
+  }
+  if (!ok) {
+    throw new Error('database is not available');
+  }
+  await client.end();
+};
+
+console.log('Waiting for postgres...');
+wait().catch(e => {
+  console.error(e);
+});
