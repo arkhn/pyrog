10f1423dbd1d3f7aa5149526f7688ee13c28053a
simonvadee <simon.vadee@gmail.com>
2020-01-08 16:07:22 +0100

    Add save button for comments

--- client-v2/src/views/Mapping/Comments/index.tsx
+++ client-v2/src/views/Mapping/Comments/index.tsx
@@ -1,65 +1,62 @@
-import * as React from "react"
-import { useQuery, useMutation } from '@apollo/react-hooks';
+import * as React from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import { useSelector } from "react-redux";
-import {
-  FormGroup,
-  TextArea,
-} from "@blueprintjs/core";
+import { FormGroup, TextArea, Button } from "@blueprintjs/core";
 
 import { IReduxStore } from "../../../types";
-
 
 // GRAPHQL
 const qCommentsForAttribute = require("src/graphql/queries/commentsForAttribute.graphql");
 const mUpdateAttribute = require("src/graphql/mutations/updateAttribute.graphql");
 
 const Comments = () => {
-
   const selectedNode = useSelector((state: IReduxStore) => state.selectedNode);
 
-  const { data, loading } =
-    useQuery(qCommentsForAttribute, {
-      variables: {
-        attributeId: selectedNode.attribute.id
-      },
-      skip: !selectedNode.attribute.id
-    })
-  const [updateAttribute] = useMutation(mUpdateAttribute)
+  const { data, loading } = useQuery(qCommentsForAttribute, {
+    variables: {
+      attributeId: selectedNode.attribute.id
+    },
+    skip: !selectedNode.attribute.id
+  });
+  const [updateAttribute] = useMutation(mUpdateAttribute);
 
-  const [comments, setComments] = React.useState("")
+  const [comments, setComments] = React.useState("");
 
   React.useEffect(() => {
-    setComments(
-      data && data.attribute.comments
-        ? data.attribute.comments
-        : ""
-      )
-  }, [selectedNode, loading])
+    setComments(data && data.attribute.comments ? data.attribute.comments : "");
+  }, [selectedNode, loading]);
 
   return (
-    <div>
+    <div id="comment">
       <FormGroup label={<h3>Comments</h3>}>
-        <div>
+        <div id="comment-input">
           <TextArea
             className={"bp3-fill"}
             value={comments}
             disabled={loading || !selectedNode.attribute.id}
             onChange={e => {
-              setComments(e.target.value)
+              setComments(e.target.value);
+            }}
+          />
+          <Button
+            id="save-comment-button"
+            onClick={() => {
               updateAttribute({
                 variables: {
                   attributeId: selectedNode.attribute.id,
                   data: {
-                    comments: e.target.value
+                    comments
                   }
-                },
-              })
+                }
+              });
             }}
-          />
+          >
+            Save
+          </Button>
         </div>
       </FormGroup>
     </div>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
