c2ec2cd5eb5a11d85f0fb3d1198c4394ee18ccdb
Simon Vadée <simon.vadee@gmail.com>
2020-01-15 15:22:52 +0100

    Handle versionning in mapping export/import (#113)

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -4,3 +4,6 @@ export const { APP_SECRET, SCHEMAS_DIR } = process.env
 
 const IV_LENGTH = 16 // For AES, this is always 16
 export const IV = randomBytes(IV_LENGTH)
+
+export const MAPPING_VERSION_1 = 1
+export const CURRENT_MAPPING_VERSION = MAPPING_VERSION_1
