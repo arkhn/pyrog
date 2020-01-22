b63d941ea85fea09563f10c28dd7f1686546c2c1
simonvadee <simon.vadee@gmail.com>
2020-01-22 11:32:07 +0100

    integrate caching for structuredefs

--- server/src/fhir/definitions.ts
+++ server/src/fhir/definitions.ts
@@ -1,32 +1,107 @@
 import { Photon } from '@prisma/photon'
+import NodeCache from 'node-cache'
+import set from 'lodash.set'
+
+import { StructureDefinition } from 'types'
 import { HL7_AUTHOR } from '../constants'
 
 const photon = new Photon()
+const cache = new NodeCache()
 
-const structurize = (def: any): any => {}
+const metaPrefix = '$meta'
 
-const set = (key: string, value: any): void => {}
+// Gets a definition from the cache.
+export const getDefinition = (key: string) => cache.get(key)
 
-const cacheDefinitions = (definitions: any[]): void => {
-  for (const def of definitions) {
-    const struct = structurize(def)
-    set('key', struct)
-  }
-}
-
-// compares StructureDefinitions from the database with those from the filesystem.
-//      - loads definitions from the database
-//      - cache a structured version of the definition in an in-memory cache
+// Loads definitions from the database and cache a structured
+// version of the definition in an in-memory cache.
 export const bootstrapDefinitions = async () => {
+  const cacheDefinition = (fhirStructureDefinition: any) => {
+    const struct = structurize(fhirStructureDefinition.resource)
+
+    // Use id as key. If it isn't present, use url
+    const { id, url } = fhirStructureDefinition.resource
+
+    if (!id && !url) {
+      throw new Error('Structure definition has no id nor url field.')
+    }
+
+    // Put obj in key value store
+    cache.set(id || url, struct)
+  }
+
   console.log('Bootstrapping standard FHIR definitions...')
   const standardDefinitions = await photon.structureDefinitions.findMany({
     where: { author: HL7_AUTHOR },
   })
-  await cacheDefinitions(standardDefinitions)
+  for (const def of standardDefinitions) {
+    cacheDefinition(def)
+  }
 
   console.log('Bootstrapping custom FHIR definitions...')
   const customDefinitions = await photon.structureDefinitions.findMany({
     where: { author: { not: HL7_AUTHOR } },
   })
-  await cacheDefinitions(customDefinitions)
+  for (const def of customDefinitions) {
+    cacheDefinition(def)
+  }
 }
+
+const structurize = (definition: any): StructureDefinition => {
+  if (!definition.snapshot) {
+    throw new Error('Snapshot is needed in the structure definition.')
+  }
+
+  // Create the new custom structure
+  var customStruct = {} as StructureDefinition
+
+  Object.keys(definition)
+    .filter(el => structureFieldsWhiteList.includes(el))
+    .forEach(key => set(customStruct, `${metaPrefix}.${key}`, definition[key]))
+
+  definition.snapshot.element.forEach((element: any, index: number) => {
+    // From the root, we only need the cardinality and constraints
+    if (index === 0) {
+      Object.keys(element)
+        .filter(el => rootProperties.includes(el))
+        .forEach(key => set(customStruct, `${metaPrefix}.${key}`, element[key]))
+    } else if (definition.kind !== 'primitive-type') {
+      // If the structure defines a primitive type (one which we don't need to unroll in UI)
+      // we don't need the properties field, we only need the cardinality and constraints from the root
+
+      const elementName = element.id.split('.').pop()
+
+      // We skip some elements we don't need as id, extension
+      if (!elementBlackList.includes(elementName)) {
+        // Add element fields
+        Object.keys(element)
+          .filter(el => elementFieldsWhiteList.includes(el))
+          .forEach(key =>
+            set(customStruct, `${elementName}.${key}`, element[key]),
+          )
+      }
+    }
+  })
+
+  return customStruct
+}
+
+// keys to filter out
+const rootProperties = ['min', 'max', 'constraint']
+const structureFieldsWhiteList = [
+  'id',
+  'url',
+  'name',
+  'description',
+  'kind',
+  'baseDefinition',
+  'derivation',
+]
+const elementFieldsWhiteList = [
+  'definition',
+  'min',
+  'max',
+  'type',
+  'constraint',
+]
+const elementBlackList = ['id', 'extension']
