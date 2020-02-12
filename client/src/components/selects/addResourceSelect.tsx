52cfc9b94dc17d67adc2f00c6daaa577e914d2fa
simonvadee <simon.vadee@gmail.com>
2020-02-12 11:28:08 +0100

    make default profile visible

--- client/src/components/selects/addResourceSelect.tsx
+++ client/src/components/selects/addResourceSelect.tsx
@@ -45,6 +45,7 @@ const AddResourceSelect = ({
       <MenuItem
         key={profile.id}
         onClick={handleClick}
+        label={profile.id === profile.type ? 'Default' : ''}
         text={
           <span>
             <strong>{profile.name}</strong>
@@ -67,7 +68,7 @@ const AddResourceSelect = ({
         rightIcon="caret-right"
         filterItems={filterByName}
         inputItem={resource}
-        items={sortItems(resource.profiles || [])}
+        items={sortItems(resource.profiles || [], resource.id)}
         onChange={onChange}
         popoverProps={{
           autoFocus: true,
@@ -86,20 +87,33 @@ const AddResourceSelect = ({
     return resource.name.toLowerCase().indexOf(query.toLowerCase()) >= 0;
   };
 
-  const sortAlphabetically = (item1: Profile, item2: Profile): number => {
+  const sortAlphabetically = (
+    item1: Profile,
+    item2: Profile,
+    first: string | undefined
+  ): number => {
+    if (item1.id === first) return -1;
+    if (item2.id === first) return 1;
+
     const name1 = item1.name.toLowerCase();
     const name2 = item2.name.toLowerCase();
+
     if (name1 < name2) return -1;
     if (name1 > name2) return 1;
     return 0;
   };
 
-  const sortItems = (profiles: Profile[]): Profile[] => {
+  // If the second argument is provided, the sort funcion puts
+  // the resource which id matches on top of the list.
+  const sortItems = (
+    profiles: Profile[],
+    first: string | undefined = undefined
+  ): Profile[] => {
     if (loading) {
       return [];
     }
 
-    return profiles.sort((p1, p2) => sortAlphabetically(p1, p2));
+    return profiles.sort((p1, p2) => sortAlphabetically(p1, p2, first));
   };
 
   return (
