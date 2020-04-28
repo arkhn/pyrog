a5a5ed55133561bb9f310803d1a32173942c7841
elsiehoffet-94 <56258130+elsiehoffet-94@users.noreply.github.com>
2020-04-28 10:23:36 +0200

    Build and publish pyrog-client for every environment in github actions (#273)
    * add build and publish steps in github actions
    
    * fix indent
    
    * rename env generating script
    
    Co-authored-by: Simon Vadée <simon.vadee@gmail.com>

---
+++ client/scripts/generate_client_env.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+echo "STAGE=.$STAGE
+PORT=1729
+REACT_APP_HTTP_BACKEND_URL=http://$IP/pyrog
+REACT_APP_FHIRPIPE_URL=http://$IP/pipe
+REACT_APP_PAGAI_URL=http://$IP/pagai
+REACT_APP_CLEANING_SCRIPTS_URL=http://$IP/scripts
+REACT_APP_FHIR_API_URL=http://$IP/api
+"
