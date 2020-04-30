da8b8b1b49244b7a507139eb251e681642c8fa50
Jasopaum <jason.paumier@gmail.com>
2020-04-30 17:37:25 +0200

    remove typo

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -65,7 +65,6 @@ const Navbar = ({ exportMapping }: Props) => {
           onClick={() => setIsDrawerOpen(true)}
           text="Database"
         />
-        q
         <Button
           icon="export"
           minimal={true}
