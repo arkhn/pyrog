54656e4aa9e5cfb26d43389f8570bb0894d7b788
elsiehoffet-94 <56258130+elsiehoffet-94@users.noreply.github.com>
2020-06-30 18:20:22 +0200

    adapt scripts to new pyrog backend api url

--- client/scripts/generate_client_env.sh
+++ client/scripts/generate_client_env.sh
@@ -3,7 +3,7 @@
 
 echo "STAGE=.$STAGE
 PORT=1729
-REACT_APP_HTTP_BACKEND_URL=http://$IP/pyrog
+REACT_APP_HTTP_BACKEND_URL=http://$IP/pyrog-api
 REACT_APP_RIVER_URL=http://$IP/river
 REACT_APP_PAGAI_URL=http://$IP/pagai
 REACT_APP_CLEANING_SCRIPTS_URL=http://$IP/scripts
