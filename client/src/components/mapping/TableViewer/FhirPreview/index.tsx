743b939ac369864e634eeff4e7f3b4ed815596c0
Jasopaum <jason.paumier@gmail.com>
2020-07-29 19:28:28 +0200

    fix bug reclick on same preview

--- client/src/components/mapping/TableViewer/FhirPreview/index.tsx
+++ client/src/components/mapping/TableViewer/FhirPreview/index.tsx
@@ -1,69 +1,37 @@
 import React from 'react';
 import { Spinner } from '@blueprintjs/core';
-import axios from 'axios';
-import { useSelector } from 'react-redux';
-import { IReduxStore } from 'types';
-
-import { RIVER_URL } from '../../../../constants';
 
 interface Props {
-  rowId: number;
-  resourceId: string;
+  previewData: {
+    instances: any;
+    errors: string[];
+  };
+  loading: boolean;
 }
 
-const FhirPreview = ({ rowId, resourceId }: Props) => {
-  const toaster = useSelector((state: IReduxStore) => state.toaster);
-  const [fhirObject, setFhirObject] = React.useState(undefined as any);
-  const [validationErrors, setValidationErrors] = React.useState(
-    [] as string[]
-  );
-  const [loading, setLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    const fetchPreview = async () => {
-      try {
-        setLoading(true);
-        const res = await axios.post(
-          `${RIVER_URL}/preview`,
-          {
-            resource_id: resourceId,
-            primary_key_values: [rowId]
-          },
-          { headers: { 'Content-Type': 'application/json' } }
-        );
-        setFhirObject(res.data.instances);
-        setValidationErrors(res.data.errors);
-      } catch (err) {
-        toaster.show({
-          message: err.response ? err.response.data : err.message,
-          intent: 'danger',
-          icon: 'warning-sign',
-          timeout: 6000
-        });
-      }
-      setLoading(false);
-    };
-    fetchPreview();
-  }, [rowId, resourceId, toaster]);
-
+const FhirPreview = ({ previewData, loading }: Props) => {
   if (loading) return <Spinner />;
 
   const renderValidationErrors = () => (
     <React.Fragment>
       <h3> Validation errors </h3>
-      {validationErrors.map((error, ind) => (
+      {previewData.errors.map((error, ind) => (
         <pre key={ind}>{error}</pre>
       ))}
     </React.Fragment>
   );
 
-  return (
+  return previewData ? (
     <div>
-      {validationErrors.length > 0 && renderValidationErrors()}
-      <h3> Fhir instance preview </h3>
-      <pre>{JSON.stringify(fhirObject, null, 4)}</pre>
+      {previewData.errors.length > 0 && renderValidationErrors()}
+      {previewData.instances!! && (
+        <>
+          <h3> Fhir instance preview </h3>
+          <pre>{JSON.stringify(previewData.instances, null, 4)}</pre>
+        </>
+      )}
     </div>
-  );
+  ) : null;
 };
 
 export default FhirPreview;
