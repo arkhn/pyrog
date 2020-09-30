4ac41486e18dd1caccb294b29b43f6a5c8ba0b70
Jasopaum <jason.paumier@gmail.com>
2020-09-30 12:09:08 +0200

    add oauth env vars to script

--- client/scripts/generate_client_env.sh
+++ client/scripts/generate_client_env.sh
@@ -8,4 +8,12 @@ REACT_APP_RIVER_URL=http://$IP/river
 REACT_APP_PAGAI_URL=http://$IP/pagai
 REACT_APP_CLEANING_SCRIPTS_URL=http://$IP/scripts
 REACT_APP_FHIR_API_URL=http://$IP/api
+REACT_APP_CLIENT_ID=pyrog-client
+REACT_APP_CLIENT_SECRET=pyrog-client
+REACT_APP_AUTH_URL=http://$IP/hydra/oauth2/auth
+REACT_APP_TOKEN_URL=http://$IP/hydra/oauth2/token
+REACT_APP_REVOKE_URL=http://$IP/hydra/oauth2/revoke
+REACT_APP_LOGOUT_URL=http://$IP/hydra/oauth2/sessions/logout
+REACT_APP_LOGIN_REDIRECT_URL=http://$IP/pyrog/
+REACT_APP_LOGOUT_REDIRECT_URL=http://$IP/pyrog/login
 "
