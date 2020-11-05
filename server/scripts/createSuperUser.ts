a33abc919511cec01ec080afb649ecac103e3f69
Teva RIOU <teva.riou@gmail.com>
2020-11-05 18:11:35 +0100

    Signup superuser to idp

--- server/scripts/createSuperUser.ts
+++ server/scripts/createSuperUser.ts
@@ -1,16 +1,31 @@
 import { PrismaClient } from '@prisma/client'
+import axios from 'axios'
 
 const prismaClient = new PrismaClient()
 
-const { SUPERUSER_EMAIL, SUPERUSER_PASSWORD } = process.env
+const {
+  SUPERUSER_EMAIL,
+  SUPERUSER_PASSWORD,
+  IDENTITY_PROVIDER_URL,
+} = process.env
 
 const main = async () => {
   if (!SUPERUSER_PASSWORD) throw Error('superuser password is required')
+  const user = {
+    name: 'admin',
+    password: SUPERUSER_PASSWORD as string,
+    email: SUPERUSER_EMAIL || 'admin@arkhn.com',
+  }
+  let form = new FormData()
+  form.append('name', user.name)
+  form.append('password', user.password)
+  form.append('email', user.email)
+  const res = await axios.post(`${IDENTITY_PROVIDER_URL}/signup`, form)
+  if (res.status === 500)
+    throw Error('identity provider failed to create superuser')
   await prismaClient.user.create({
     data: {
-      name: 'admin',
-      password: SUPERUSER_PASSWORD as string,
-      email: SUPERUSER_EMAIL || 'admin@arkhn.com',
+      ...user,
       role: 'ADMIN',
     },
   })
@@ -23,5 +38,5 @@ prismaClient
   .then(() => process.exit(0))
   .catch(err => {
     console.error(err)
-    prismaClient.$disconnect().then(() => process.exit(+!SUPERUSER_PASSWORD))
+    prismaClient.$disconnect().then(() => process.exit(1))
   })
