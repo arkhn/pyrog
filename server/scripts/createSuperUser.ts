2bb6e7d67d981feb314ce1565c3db9c7c6e6f411
Teva RIOU <teva.riou@gmail.com>
2020-11-05 16:52:55 +0100

    Add script to add a superuser

---
+++ server/scripts/createSuperUser.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+
+const prismaClient = new PrismaClient()
+
+const { SUPERUSER_EMAIL, SUPERUSER_PASSWORD } = process.env
+
+const main = async () => {
+  if (!SUPERUSER_PASSWORD) throw Error('superuser password is required')
+  await prismaClient.user.create({
+    data: {
+      name: 'admin',
+      password: SUPERUSER_PASSWORD as string,
+      email: SUPERUSER_EMAIL || 'admin@arkhn.com',
+    },
+  })
+}
+
+prismaClient
+  .$connect()
+  .then(() => main())
+  .then(() => prismaClient.$disconnect())
+  .then(() => process.exit(0))
+  .catch(err => {
+    console.error(err)
+    prismaClient.$disconnect().then(() => process.exit(+!SUPERUSER_PASSWORD))
+  })
