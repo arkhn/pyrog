88e4f74e3f19209dad0d79de4c4b41cbf00b2fb7
simonvadee <simon.vadee@gmail.com>
2020-11-24 11:32:34 +0100

    [auth] upsert superuser and skip IDP

--- server/scripts/createSuperUser.ts
+++ server/scripts/createSuperUser.ts
@@ -11,28 +11,37 @@ const {
 } = process.env
 
 const main = async () => {
-  if (!SUPERUSER_PASSWORD) throw Error('superuser password is required')
-  const res = await prismaClient.user.count({
-    where: { name: 'admin', email: SUPERUSER_EMAIL },
-  })
-  if (res) {
-    console.warn(`user ${SUPERUSER_EMAIL} already exists, skipping...`)
-    return
-  }
+  if (!SUPERUSER_PASSWORD)
+    throw Error('SUPERUSER_PASSWORD is required in environment')
+  if (!SUPERUSER_EMAIL)
+    throw Error('SUPERUSER_EMAIL is required in environment')
+
   const user = {
     name: 'admin',
     password: SUPERUSER_PASSWORD as string,
-    email: SUPERUSER_EMAIL || 'admin@arkhn.com',
+    email: SUPERUSER_EMAIL,
   }
-  let form = new FormData()
-  form.append('name', user.name)
-  form.append('password', user.password)
-  form.append('email', user.email)
-  await axios.post(`${IDENTITY_PROVIDER_URL}/signup`, form, {
-    headers: form.getHeaders(),
-  })
-  await prismaClient.user.create({
-    data: {
+
+  // if IDENTITY_PROVIDER_URL is provided, send a request to create the user
+  if (IDENTITY_PROVIDER_URL) {
+    let form = new FormData()
+    form.append('name', user.name)
+    form.append('password', user.password)
+    form.append('email', user.email)
+    await axios.post(`${IDENTITY_PROVIDER_URL}/signup`, form, {
+      headers: form.getHeaders(),
+    })
+  }
+
+  // upsert the user in pyrog database
+  await prismaClient.user.upsert({
+    where: {
+      email: user.email,
+    },
+    update: {
+      role: 'ADMIN',
+    },
+    create: {
       name: user.name,
       email: user.email,
       role: 'ADMIN',
